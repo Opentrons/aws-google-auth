5479c56925182eefcdedb710f01a04f09a5bd196
Sam Caldwell <samuel.caldwell@opentrons.com>
2021-12-20 23:21:33 -0600

    remove python2 support

--- setup.py
+++ setup.py
@@ -62,9 +62,8 @@ setup(
         'License :: OSI Approved :: MIT License',
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
 
     ],
 

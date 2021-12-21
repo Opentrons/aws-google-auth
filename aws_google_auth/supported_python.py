382dbe92117fc31341109d2fbbe0f4bf5edb65d2
Sam Caldwell <samuel.caldwell@opentrons.com>
2021-12-21 09:09:49 -0600

    add exit_if_supported_python

---
+++ aws_google_auth/supported_python.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+__strict__ = True
+
+from sys import version_info
+from logging import critical
+from sys import exit
+
+
+def exit_if_unsupported_python():
+    if version_info.major == 2 and version_info.minor < 7:
+        critical("%s requires Python 2.7 or higher. Please consider "
+                 "upgrading. Support for Python 2.6 and lower was "
+                 "dropped because this tool's dependencies dropped "
+                 "support.", __name__)
+        critical(f"For debugging, it appears you're running: {version_info}")
+        critical("For more information, see: "
+                 "https://github.com/cevoaustralia/aws-google-auth/"
+                 "issues/41")
+        exit(1)

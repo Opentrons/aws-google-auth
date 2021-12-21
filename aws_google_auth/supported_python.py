cf57605560a722b35fb2e89aca4a75aed56b9960
Sam Caldwell <samuel.caldwell@opentrons.com>
2021-12-21 09:19:35 -0600

    format supported_python.py

--- aws_google_auth/supported_python.py
+++ aws_google_auth/supported_python.py
@@ -13,8 +13,11 @@ def exit_if_unsupported_python():
                  "upgrading. Support for Python 2.6 and lower was "
                  "dropped because this tool's dependencies dropped "
                  "support.", __name__)
+
         critical(f"For debugging, it appears you're running: {version_info}")
+
         critical("For more information, see: "
                  "https://github.com/cevoaustralia/aws-google-auth/"
                  "issues/41")
+
         exit(1)

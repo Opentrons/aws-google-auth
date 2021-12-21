898b88b854f7d069977632dd20d80b9642a898ca
Sam Caldwell <samuel.caldwell@opentrons.com>
2021-12-21 09:32:52 -0600

    simplify main.py

--- main.py
+++ main.py
@@ -2,13 +2,12 @@
 
 __strict__ = True
 
-import sys
+from sys import argv
 from aws_google_auth.cli import cli
 
 
 def main():
-    cli_args = sys.argv[1:]
-    cli(cli_args)
+    cli(argv[1:])
 
 
 if __name__ == '__main__':

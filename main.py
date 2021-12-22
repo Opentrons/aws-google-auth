dfbe43620e49bd1cc3254498d2ba487289a1c9fc
Sam Caldwell <samuel.caldwell@opentrons.com>
2021-12-22 11:48:17 -0600

    simplify the entrypoint

--- main.py
+++ main.py
@@ -2,11 +2,45 @@
 
 __strict__ = True
 
-from aws_google_auth.cli import cli
+from sys import exit
+from sys import version_info
+from aws_google_auth.process_auth import process_auth
+from aws_google_auth.exceptions import UnsupportedPython
+from aws_google_auth.exceptions import ExpectedGoogleException
+from aws_google_auth.configuration import Configuration
+
+
+def check_python_version():
+    if version_info.major == 2 and version_info.minor < 7:
+        raise UnsupportedPython()
 
 
 def main():
-    cli()
+    """
+        Process command-line arguments and execute the program payload.
+
+        param cli_args: list
+        return: None
+    """
+    try:
+        check_python_version()
+        config = Configuration()
+        process_auth(config)
+
+    except UnsupportedPython as ex:
+        print(ex)
+        exit(3)
+
+    except ExpectedGoogleException as ex:
+        print(ex)
+        exit(2)
+
+    except KeyboardInterrupt:
+        pass
+
+    except Exception as ex:
+        print(f"unhandled exception\n{ex}")
+        exit(1)
 
 
 if __name__ == '__main__':

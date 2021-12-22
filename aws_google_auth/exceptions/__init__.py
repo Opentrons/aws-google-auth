253b495b48b9c9b2762a450f1501807960d8c5db
Sam Caldwell <samuel.caldwell@opentrons.com>
2021-12-22 11:45:34 -0600

    add new exception classes

--- aws_google_auth/exceptions/__init__.py
+++ aws_google_auth/exceptions/__init__.py
@@ -2,13 +2,17 @@
 
 __strict__ = True
 
-from sys import version_info
 from logging import critical
-from sys import exit
+from sys import version_info
 
 
-def exit_if_unsupported_python():
-    if version_info.major == 2 and version_info.minor < 7:
+class ExpectedGoogleException(Exception):
+    def __init__(self, *args):
+        super().__init__(*args)
+
+
+class UnsupportedPython(Exception):
+    def __init__(self, *args):
         critical("%s requires Python 2.7 or higher. Please consider "
                  "upgrading. Support for Python 2.6 and lower was "
                  "dropped because this tool's dependencies dropped "
@@ -19,5 +23,4 @@ def exit_if_unsupported_python():
         critical("For more information, see: "
                  "https://github.com/cevoaustralia/aws-google-auth/"
                  "issues/41")
-
-        exit(1)
+        super().__init__(*args)
